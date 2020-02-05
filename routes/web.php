594ec2bf8f172c4b0945b11eb96548163404dd7e
scarlet <scarlet>
2020-02-04 21:41:41 -0500

    Proyecto uno

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::get('/', function () {
+    return view('welcome');
+});
+      
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
